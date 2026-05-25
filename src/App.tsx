c450749bf068dfa1f64f02297fa14e41ec2e7dba
sadie100 <03sadie@gmail.com>
2026-05-25 17:48:38 +0900

    feat: 페이지 변경될 대마다 scrollTop 되게 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import CourseListPage from '@/pages/courseList'
 import EnrollPage from '@/pages/enroll'
 import EnrollDonePage from '@/pages/enrollDone'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<CourseListPage />} />
         <Route path="/enroll/:courseId" element={<EnrollPage />} />
